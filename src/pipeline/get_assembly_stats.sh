1ab9d2b0c178579c369fdd1f72e0875428ae8030
Tristan <tjang8858@rci-valafar-head-01.cm.cluster>
2025-05-05 17:12:14 -0700

    shell script that iterates over isolates, counts the number of sequences in their assembly files, and computes the total sequence length from the 06.fixstart.fasta file for each isolate.

---
+++ src/pipeline/get_assembly_stats.sh
@@ -0,0 +1,1 @@
+for isolate in $(cat isolates.txt); do echo $isolate && grep -c ">" $isolate/assembly/circ/06.fixstart.fasta &&  awk '/^>/ {if (seqlen){print seqlen}; print ;seqlen=0;next; } { seqlen += length($0)}END{print seqlen}' $isolate/assembly/circ/06.fixstart.fasta;done;
